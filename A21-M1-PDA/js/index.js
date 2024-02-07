c2eaa9621277d16bbff27085ec93f26315ca696d
Kauan Freitas <kauan.allexandre@gmail.com>
2024-02-06 21:14:34 -0300

    Perguntas ao Usuario

---
+++ A21-M1-PDA/js/index.js
@@ -0,0 +1,24 @@
+
+
+
+var frutas = [];
+for(var i = 0; i <=3; i++){
+    let frutasInserir = prompt("Digite a Fruta desejada");
+    document.write(frutasInserir, "<br>");
+    frutas.push(frutasInserir);
+} 
+
+var comidas = [];
+for(var i = 0; i <=3; i++){
+    let comidasInserir = prompt("Digite a Comida desejada");
+    document.write(comidasInserir, "<br>");
+    frutas.push(comidasInserir);
+} 
+
+var bebidas = [];
+for(var i = 0; i <=3; i++){
+    let bebidasInserir = prompt("Digite a Bebida desejada");
+    document.write(bebidasInserir, "<br>");
+    frutas.push(bebidasInserir);
+} 
+
